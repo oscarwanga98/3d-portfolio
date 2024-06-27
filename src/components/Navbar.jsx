768ef18b8d3bc0a9da91f7cf6958206749b71d2d
Oscar Wanga <87162511+oscarwanga98@users.noreply.github.com>
2024-06-27 20:59:14 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,7 +28,7 @@ const Navbar = () => {
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer flex">
-            Oscar &nbsp;<span className="sm:block hidden"> {' '}| Web Developer</span>{" "}
+            Fredrick &nbsp;<span className="sm:block hidden"> {' '}| Web Developer</span>{" "}
           </p>
         </Link>
         <ul className="list-none hidden sm:flex flex-flow gap-10">
